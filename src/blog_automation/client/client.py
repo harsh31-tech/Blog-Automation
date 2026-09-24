874f2ac709a93b4ba19059cf8d8cd8c5865a6bcf
Harsh Agarwal <harshagarwal31.creator@gmail.com>
2026-09-24 12:09:40 +0530

    done with resource reading and printing part

--- src/blog_automation/client/client.py
+++ src/blog_automation/client/client.py
@@ -8,8 +8,13 @@ async def main():
     async with client:
         print("Client connected to MCP server")
 
-        result = await client.list_tools()
-        print(result)
+        # result = await client.list_tools()
+        # print(result)
+        
+        resource = await client.list_resources()
+        read_resource = await client.read_resource("resource://blog_template")
+        print(resource)
+        print(read_resource[0].text)
 
 
 if __name__ == "__main__":
--- src/blog_automation/server/server.py
+++ src/blog_automation/server/server.py
@@ -1,4 +1,5 @@
 from fastmcp import FastMCP
+from pathlib import Path
 import asyncio
 
 mcp = FastMCP(name="Blog_Automation")
@@ -7,6 +8,15 @@ mcp = FastMCP(name="Blog_Automation")
 @mcp.tool
 async def add(a: int, b: int) -> int:
     return a + b
+
+
+@mcp.resource(
+    "resource://blog_template",
+    description="use this blog template to generate the blog structure",
+)
+async def blog_template():
+    file_path = Path("resources/blog_template.md")
+    return file_path.read_text(encoding="utf-8")
 
 
 if __name__ == "__main__":
